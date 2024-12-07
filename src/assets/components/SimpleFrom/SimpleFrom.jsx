19be977071d31d2b40b664633750f57c44790477
Anay Saha <anaysahapaban@gmail.com>
2024-12-08 02:47:53 +0600

    add

--- src/assets/components/SimpleFrom/SimpleFrom.jsx
+++ src/assets/components/SimpleFrom/SimpleFrom.jsx
@@ -4,16 +4,21 @@ const SimpleFrom = () => {
 
     const handleSubmit = e =>{
     e.preventDefault();
+    console.log(e.target.name.value);
+    console.log(e.target.email.value )
+    console.log('form submitted')
     }
     return (
         <div>
                 <form onSubmit={handleSubmit}>
-                    <input type="text" />
+                    <input type="text" name="name" />
+                    <br />
+                    <input type="email" name="email" />
                     <br />
                     <input type="submit" value="Submit" />
                 </form>
         </div>
     );
 };
-
+ 
 export default SimpleFrom;
