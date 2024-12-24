4036869675b5b8ed6fce429748d5639f6644dede
Anay Saha <anaysahapaban@gmail.com>
2024-12-24 18:41:31 +0600

    Context

--- src/assets/components/Dad/Dad.jsx
+++ src/assets/components/Dad/Dad.jsx
@@ -1,9 +1,15 @@
+import Myself from "./Myself/Myself";
 
 
-const Dad = () => {
+const Dad = (asset) => {
     return (
         <div>
            <h2>Dad</h2> 
+           <section className="flex">
+            <Myself asset={asset}></Myself>
+            <Brother></Brother>
+            <Sister></Sister>
+           </section>
         </div>
     );
 };
--- src/assets/components/Dad/Myself/Myself.jsx
+++ src/assets/components/Dad/Myself/Myself.jsx
@@ -1,11 +1,14 @@
 
 
-const Myself = () => {
+const Myself = ({asset}) => {
     return (
         <div>
-            
+            <h2> Myself</h2>
+            <section className="flex">
+            <Special></Special>
+            </section>
         </div>
-    );
+    ); 
 };
 
 export default Myself;
